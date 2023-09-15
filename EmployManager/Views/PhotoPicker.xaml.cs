ca7dddad83982c322063827a804061b576874637
Михаил <110668707+javaBoy2500Genius@users.noreply.github.com>
2023-09-15 15:14:52 +0600

    update for mosha

---
+++ EmployManager/Views/PhotoPicker.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EmployManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PhotoPicker : ContentView
+    {
+        public PhotoPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
