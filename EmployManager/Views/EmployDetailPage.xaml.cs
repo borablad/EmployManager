a3fc3bcaf9f218602ac726447287496449f29205
Bora_blad <96587810+borablad@users.noreply.github.com>
2023-09-15 15:14:28 +0600

    bora commit hahahhaha

--- EmployManager/Views/EmployDetailPage.xaml.cs
+++ EmployManager/Views/EmployDetailPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿namespace EmployManager.Views;
 using EmployManager.ViewModels;
+using Microsoft.Maui.Storage;
+using static EmployManager.Models.Enums;
+
 public partial class EmployDetailPage : ContentPage
 {
 	private EmployDetailViewModel vm;
@@ -16,7 +19,14 @@ public partial class EmployDetailPage : ContentPage
 		vm.OnAppering();
     }
 
-    void Picker_SelectedIndexChanged(System.Object sender, System.EventArgs e)
-    {
+    async void Picker_SelectedIndexChanged(System.Object sender, System.EventArgs e)
+	{
+		var role = MembersRole.User;
+        if (RolePicker.SelectedIndex == 0)
+            await vm.SelectRole(MembersRole.Admin);
+        else if (RolePicker.SelectedIndex == 1)
+            await vm.SelectRole(MembersRole.Manager);
+        else if (RolePicker.SelectedIndex == 2)
+           await vm.SelectRole(MembersRole.User);
     }
 }
